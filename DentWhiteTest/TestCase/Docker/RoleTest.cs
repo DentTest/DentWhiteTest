6d8bb44adb484703a576cf535c725952b534bd63
bicheno <1135064577@qq.com>
2017-10-16 13:18:48 +0800

    添加测试脚本：点击角色管理菜单，加载所有角色；输入角色名称，点击查询按钮，加载该角色；角色名称为空，点击查询按钮，加载所有角色。

--- DentWhiteTest/TestCase/Docker/RoleTest.cs
+++ DentWhiteTest/TestCase/Docker/RoleTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TestStack.White.UIItems;
 using TestStack.White.UIItems.Finders;
@@ -21,13 +22,27 @@ namespace DentWhiteTest.TestCase
             try
             {
                 var startTime = DateTime.Now;
+
                 //在菜单页选择角色管理
                 Button tlRoleManagement = appWin.Get<Button>(SearchCriteria.ByAutomationId("tlRoleManagement"));
                 tlRoleManagement.Click();
+
                 var endTime = DateTime.Now;
 
-                msg = "测试【点击角色管理菜单，加载所有角色】--通过，用时：" + (endTime - startTime).TotalSeconds;
-                return true;
+                string error_info = string.Empty;
+                try
+                {
+                    //捕捉报错信息，如果能捕捉到，则测试未通过
+                    error_info = appWin.Get<Label>(SearchCriteria.ByText("查询角色列表异常！")).ToString();
+                    msg = "测试【点击角色管理菜单，加载所有角色】--未通过，" + error_info + "用时：" + (endTime - startTime).TotalSeconds;
+                    return false;
+
+                }
+                catch
+                {
+                    msg = "测试【点击角色管理菜单，加载所有角色】--通过，用时：" + (endTime - startTime).TotalSeconds;
+                    return true;
+                }
             }
             catch (Exception e)
             {
@@ -45,11 +60,30 @@ namespace DentWhiteTest.TestCase
             {
                 var startTime = DateTime.Now;
 
+                //输入角色名称
+                TextBox txtSearchRole = appWin.Get<TextBox>(SearchCriteria.ByAutomationId("txtSearchRole"));
+                txtSearchRole.BulkText = "超级管理员";
+
+                //点击查询按钮
+                Button btnSearch = appWin.Get<Button>(SearchCriteria.ByAutomationId("btnSearch"));
+                btnSearch.Click();
 
                 var endTime = DateTime.Now;
 
-                msg = "";
-                return true;
+                string error_info = string.Empty;
+                try
+                {
+                    //捕捉报错信息，如果能捕捉到，则测试未通过
+                    error_info = appWin.Get<Label>(SearchCriteria.ByText("查询角色列表异常！")).ToString();
+                    msg = "测试【输入角色名称，点击查询按钮，加载该角色】--未通过，" + error_info + "用时：" + (endTime - startTime).TotalSeconds;
+                    return false;
+
+                }
+                catch
+                {
+                    msg = "测试【输入角色名称，点击查询按钮，加载该角色】--通过，用时：" + (endTime - startTime).TotalSeconds;
+                    return true;
+                }
             }
             catch (Exception e)
             {
@@ -67,11 +101,30 @@ namespace DentWhiteTest.TestCase
             {
                 var startTime = DateTime.Now;
 
+                //角色名称为空
+                TextBox txtSearchRole = appWin.Get<TextBox>(SearchCriteria.ByAutomationId("txtSearchRole"));
+                txtSearchRole.Text = "";
+
+                //点击查询按钮
+                Button btnSearch = appWin.Get<Button>(SearchCriteria.ByAutomationId("btnSearch"));
+                btnSearch.Click();
 
                 var endTime = DateTime.Now;
 
-                msg = "";
-                return true;
+                string error_info = string.Empty;
+                try
+                {
+                    //捕捉报错信息，如果能捕捉到，则测试未通过
+                    error_info = appWin.Get<Label>(SearchCriteria.ByText("查询角色列表异常！")).ToString();
+                    msg = "测试【角色名称为空，点击查询按钮，加载所有角色】--未通过，" + error_info + "用时：" + (endTime - startTime).TotalSeconds;
+                    return false;
+
+                }
+                catch
+                {
+                    msg = "测试【角色名称为空，点击查询按钮，加载所有角色】--通过，用时：" + (endTime - startTime).TotalSeconds;
+                    return true;
+                }
             }
             catch (Exception e)
             {
